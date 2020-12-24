7fde3e0dfeb8cefa51f117d79e1bd32dc389f33d
Nicolas Eisemann <71433649+NicholasEisemann@users.noreply.github.com>
2020-12-24 13:33:01 +0300

    changed Wallpaper

--- MyRoom/Wallpaper.cs
+++ MyRoom/Wallpaper.cs
@@ -6,5 +6,22 @@ namespace MyRoom
 {
     class Wallpaper
     {
+        void Standart()
+        {
+            int Width = 53;   //cm
+            int Length = 1000;  //сm
+        }
+
+        void European()
+        {
+            int Width = 70;
+            int Length = 1000;
+        }
+
+        void Meters()
+        {
+            int Width = 106;
+            int Length = 1000;
+        }
     }
 }
